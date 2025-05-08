5d3979f3e47f76914badf16fb62ec96aa979bacf
atmosphericalsilva <fedyadobrik@gmail.com>
2025-05-08 18:50:42 +0200

    Added error handling and expanded requirement check (with returns)

--- main.py
+++ main.py
@@ -4,32 +4,50 @@ from random import randint
 # first task:
 def get_days_from_today(user_input):
 
-    current_date = datetime.now()
+    current_date = datetime.today()
 
-    # turning string input into a date:
-    user_date = datetime.strptime(user_input, '%Y-%m-%d')
+    try:
+        # turning string input into a date:
+        user_date = datetime.strptime(user_input, '%Y-%m-%d')
 
-    # calculating the difference and printing the result:
-    difference = current_date - user_date
-    print(f'this date was {difference.days} days ago')
+        # calculating the difference and printing the result:
+        difference = current_date - user_date
+        return difference.days
+    
+    # error handle:
+    except ValueError:
+        return None
 
 # second task:
-def get_numbers_ticket(min, max, quantity):
+def get_numbers_ticket(min_str, max_str, quantity_str):
 
     ticket_nums = set()
 
-    # checking the requirements:
-    if min > 0 and max < 1001:
+    try:
+        
+        # turning string into integer
+        min = int(min_str)
+        max = int(max_str)
+        quantity = int(quantity_str)
+
+        delta = max - min
+
+        # checking the requirements:
+        if min > 0 and max < 1001 and max >= min and quantity <= delta + 1:
 
         # adding unique numbers until the set is filled:
-        while len(ticket_nums) != quantity:
-            ticket_nums.add(randint(min, max))
-        print(sorted(list(ticket_nums)))   
-    else:
-        print(list(ticket_nums))
+            while len(ticket_nums) != quantity:
+                ticket_nums.add(randint(min, max))
+            return sorted(list(ticket_nums))
+        else:
+            return list(ticket_nums)
+        
+    # correct format check:
+    except ValueError:
+        return None
 
 # first task call:
-get_days_from_today(user_input = input('input your date (Y-m-d): '))
+print(get_days_from_today(user_input = input('input your date (Y-m-d): ')))
 
 # second task call:
-get_numbers_ticket(min = int(input('min: ')), max = int(input('max: ')), quantity = int(input('quantity: ')))
+print(get_numbers_ticket(min_str = input('min: '), max_str = input('max: '), quantity_str = input('quantity: ')))
